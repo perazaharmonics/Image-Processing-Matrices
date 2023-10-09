541c106d0974d4b3ba9289141ba2240edca6a4e4
Enrique <141517235+perazaharmonics@users.noreply.github.com>
2023-10-09 12:18:12 -0400

    Updated Weiner Filter

--- mWeinerFilter_Image.m
+++ mWeinerFilter_Image.m
@@ -1,9 +1,10 @@
-me = imread("Lena2.JPG");
+me = imread("Lena2.jpg");
 me_2 = rgb2gray(me);
+figure, imshow(me_2)
 noise = imnoise(me_2, 'gaussian', 0.1);
-len = 3;
-theta = 0.5;
-PSF = fspecial('motion', len, theta);
+len = 7;
+theta = 10;
+PSF = fspecial('gaussian', len, theta);
 g = imfilter(me_2, PSF, 'circular');
 g = g + noise;
 figure, imshow(g);
@@ -27,20 +28,23 @@ figure, imshow(frest2)
 
 % Lucy Richardson Filter
 
-g = checkerboard(8);
+% g = checkerboard(8);
 
  
-PSF = fspecial('gaussian', 7, 10);
-SD = 0.01;
-g = imnoise(imfilter(g, PSF), 'gaussian', 0, SD^2);
-DAMPAR = 10*SD;
+% PSF = fspecial('gaussian', 7, 10);
+% SD = 0.1;
+% g = imnoise(imfilter(g, PSF), 'gaussian', 0, SD^2);
+% DAMPAR = 10*SD;
+% 
+% LIM = ceil(size(PSF, 1) / 2);
+% WEIGHT = zeros(size(g));
+% WEIGHT(LIM + 1: end - LIM, LIM + 1:end - LIM) = 1;
+% NUMIT = 5;
+% 
+% 
+% 
+% f5 = deconvlucy(g, PSF, NUMIT, DAMPAR, WEIGHT);
+% figure, imshow(f5)
+% 
+% 
 
-LIM = ceil(size(PSF, 1) / 2);
-WEIGHT = zeros(size(g));
-WEIGHT(LIM + 1: end - LIM, LIM + 1:end - LIM) = 1;
-NUMIT = 5;
-
- 
-
-f5 = deconvlucy(g, PSF, NUMIT, DAMPAR, WEIGHT);
-figure, imshow(f5)
